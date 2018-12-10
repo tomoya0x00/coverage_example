5a6d9634e9a87f30bfbe6c3138fd0ef0b05dfc80
tomoya.miwa <tomoya.miwa@dena.com>
2018-12-10 14:24:39 +0900

    Add initial android project

---
+++ app/src/main/java/com/github/tomoya0x00/coverageeexample/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.github.tomoya0x00.coverageeexample
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
